85c38650999c86c02d1af1b947b02d81224f6711
Tengfei Mu <frankluck52@gmail.com>
2017-10-18 20:43:30 -0700

    add preprocrule region

--- bin/stopserver.sh
+++ bin/stopserver.sh
@@ -4,4 +4,4 @@ export CLASSPATH=$GF_HOME/lib/*
 export PATH=$GF_HOME/bin:$PATH
 export JAVA_ARGS="-Dgfsh.log-level=info -Dgfsh.log-dir=$GF_HOME/logs"
 
-gfsh --e "connect --locator=127.0.0.1[55221]" --e "stop server --name=gfserver"
+gfsh --e "connect --locator=127.0.0.1[30607]" --e "stop server --name=gfserver"
